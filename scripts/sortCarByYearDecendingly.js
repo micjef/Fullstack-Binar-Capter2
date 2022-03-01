6fd5e48ac5dfc5b8ddb0ab36f6411dc739f65241
micjef <michaeljeffrysetiawan@gmail.com>
2022-03-01 12:22:21 +0700

    create descending

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -1,4 +1,4 @@
-function sortCarByYearAscendingly(cars) {
+function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
@@ -6,8 +6,13 @@ function sortCarByYearAscendingly(cars) {
   // Apa itu side effect?
   const result = [...cars];
 
+  const resultDes = [...cars]
+
   // Tulis code-mu disini
+  //sort cars by year descendingly
+  resultDes.sort((a, b) => b.year - a.year);
+  console.log(resultDes);
 
   // Rubah code ini dengan array hasil sorting secara descending
-  return result;
+  return resultDes;
 }
